ea22044680969c79f9552556b078f138718a8eaf
Mushfiq1060 <60137904+Mushfiq1060@users.noreply.github.com>
2020-11-03 18:57:35 +0600

    Update and rename F - Palindromic Subsequence.cpp to lexicographically minimum Palindromic Subsequence.cpp

--- lexicographically minimum Palindromic Subsequence.cpp
+++ lexicographically minimum Palindromic Subsequence.cpp
@@ -48,3 +48,50 @@ int main()
     return 0;
 }
 
+
+/***Another Solution***/
+
+
+#include<bits/stdc++.h>
+#define ll long long  
+using namespace std;
+string str;
+string dp[1010][1010];
+string lex_mini(string a,string b)
+{
+    if(a.size()==b.size())
+    {
+        if(a<b)
+            return a;
+        return b;
+    }
+    if(a.size()>b.size())
+        return a;
+    return b;
+}
+string func(int i,int j)
+{
+    if(i>=j)
+    {
+        string s="";
+        if(i==j)
+            return str[i]+s;
+        return "";
+    }
+    if(dp[i][j]!="")
+        return dp[i][j];
+    if(str[i]==str[j])
+        return dp[i][j]=str[i]+func(i+1,j-1)+str[j];     
+    return dp[i][j]=lex_mini(func(i+1,j),func(i,j-1));
+}
+int main()
+{   
+    while(cin>>str)
+    {
+        for(int i=0;i<1010;i++)
+            for(int j=0;j<1010;j++)
+                dp[i][j]="";
+        cout<<func(0,str.size()-1)<<endl;
+    }
+    return 0;
+}
